972331e3d8e50da5da17f444a58bedddf10aa977
kajalshukla521 <kajalshukl921@gmail.com>
2024-11-11 15:03:29 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,46 +4,33 @@ import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 import Forget from "./components/Forget";
 import PhoneForgetPassword from "./components/PhoneForgetPassword";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
+import { BrowserRouter, createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const App = () => {
-  const myRoutes = createBrowserRouter(
-    createRoutesFromElements(
-      <Route>
-        <Route path="/eCommerce-reactjs" element={<Login />} />
-        <Route path="/eCommerce-reactjs/sign-up" element={<SignUp />} />
-        <Route
-          path="/eCommerce-reactjs/forget-password"
-          element={<Forget />}
-        />
-        <Route
-          path="/eCommerce-reactjs/forget-password-phone-number"
-          element={<PhoneForgetPassword />}
-        />
-      </Route>
-    )
-  );
+  const myRoutes = createBrowserRouter([
+    {
+      path: "/eCommerce-reactjs",
+      element: <Login />,
+    },
+    {
+      path: "/eCommerce-reactjs/sign-up",
+      element: <SignUp />,
+    },
+    {
+      path: "/eCommerce-reactjs/forget-password",
+      element: <Forget />,
+    },
+    {
+      path: "/eCommerce-reactjs/forget-password-phone-number",
+      element: <PhoneForgetPassword />,
+    },
+  ]);
 
-  return <RouterProvider router={myRoutes} />;
+  return (
+    <BrowserRouter basename="/eCommerce-reactjs">
+      <RouterProvider router={myRoutes} />
+    </BrowserRouter>
+  );
 };
 
 export default App;
-
-// const App = () => {
-//   const myRouter = createBrowserRouter([
-//     {
-//       path: "/eCommerce-reactjs",
-//       element: <Login />
-//     },
-//     {
-//       path: "/eCommerce-reactjs/sign-up",
-//       element: <SignUp />
-//     }
-//   ])
-//   return <RouterProvider router={myRouter} />
-// }
